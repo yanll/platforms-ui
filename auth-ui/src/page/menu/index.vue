8ba3eb6d060b4a613ad637e3e1ac9e9e4b90b192
yanliangliang <yanliangliang123>
2017-02-27 17:54:50 +0800

    M

--- auth-ui/src/page/menu/index.vue
+++ auth-ui/src/page/menu/index.vue
@@ -18,21 +18,56 @@
     },
     data() {
       return {
-        list: []
+        list: [],
+        param: {} // 向服务器传递参数
       }
     },
     created () {
-      this.get_data()
+      var v = this;
+      console.log("-----1-----------3123-----------");
+      console.log(v);
+      console.log(v.total);
+      this.get_data();
+    },
+    computed:{
+      pages:function(){
+        console.log('2222222222')
+        var pag = [];
+        if( this.current < this.showItem ){ //如果当前的激活的项 小于要显示的条数
+          //总页数和要显示的条数那个大就显示多少条
+          var i = Math.min(this.showItem,this.allpage);
+          while(i){
+            pag.unshift(i--);
+          }
+        }else{ //当前页数大于显示页数了
+          var middle = this.current - Math.floor(this.showItem / 2 ),//从哪里开始
+            i = this.showItem;
+          if( middle >  (this.allpage - this.showItem)  ){
+            middle = (this.allpage - this.showItem) + 1
+          }
+          while(i--){
+            pag.push( middle++ );
+          }
+        }
+        return pag
+      }
     },
     methods: {
+      handleSizeChange(val) {
+        console.log(`每页1 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        this.currentPage = val;
+        console.log(`当前页1: ${val}`);
+      },
       get_data: function (params) {
         var v = this
         if (!params) params = {}
         v.$api.get('/menu/list', params, function (resp) {
-          v.list = resp.data;
-          v.page = 2;
-          v.limit = 5;
-          v.total = 30;
+          v.list = resp.data.items;
+          v.page = resp.data.paginator.page;
+          v.limit = resp.data.paginator.limit;
+          v.total = resp.data.paginator.totalCount;
         })
       },
     },
