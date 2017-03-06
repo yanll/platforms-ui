ff164e163f87fe65b24815e3b0d3dc97cda49f1c
yanliangliang <yanliangliang123>
2017-03-06 18:46:32 +0800

    M

--- auth-ui/src/pages/menu/index.vue
+++ auth-ui/src/pages/menu/index.vue
@@ -1,40 +1,34 @@
 <template>
   <div style="background-color: white">
     <navBar :navbar_title="navbar_title" :navbar_data="navbar_data"></navBar>
-    <el-table :data="list" stripe border style="width: 100%">
-      <el-table-column prop="menuName" label="菜单名称" width="180"></el-table-column>
-      <el-table-column prop="url" label="路由地址"></el-table-column>
-    </el-table>
-    <paginator @load_data="load_data" ref="paginator" :page="page" :limit="limit" :total="total"></paginator>
+    <el-tree
+      :data="tree_data"
+      :props="defaultProps"
+      show-checkbox
+      node-key="id"
+      default-expand-all
+      :expand-on-click-node="false"
+      :render-content="renderContent">
+    </el-tree>
   </div>
 </template>
 
 
 <script>
-  import Paginator from '../../components/common/Paginator.vue'
   import NavBar from '../../components/common/NavBar.vue'
 
   export default {
     components: {
-      "paginator": Paginator,
       "navBar": NavBar
     },
     data() {
       return {
         navbar_title: '首页 > 系统管理 > 菜单管理',
-        navbar_data: {
-          data: [{
-            title: '首页',
-            url: '/'
-          }, {
-            title: '系统管理',
-            url: ''
-          }, {
-            title: '菜单管理',
-            url: '../pages/menu/index.vue'
-          }]
-        },
-        list: []
+        tree_data: [],
+        defaultProps: {
+          children: 'children',
+          label: 'menu_name'
+        }
       }
     },
     created () {
@@ -42,20 +36,35 @@
     },
     computed: {},
     methods: {
+      renderContent(h, {node, data, store}) {
+        var label = h('span', [
+          h('span', node.label)
+        ]);
+        var button_span = h('span', {style: 'float:right;font-size:12px;'}, [
+          h('a', {style: 'margin-right:12px;', on: {click: this.detail(store, data)}}, '新增'),
+          h('a', {style: 'margin-right:12px;'}, '编辑'),
+          h('a', {style: 'margin-right:12px;', on: {click: this.del(store, data)}}, '删除')
+        ]);
+        return h('span', [label, button_span]);
+      },
       load_data: function (params) {
-        console.log('MENU:load_data');
         var v = this;
-        var url_ = '/menu/list';
-        if (v.$refs.paginator) {
-          var pagination = '?page=' + v.$refs.paginator.page + '&limit=' + v.$refs.paginator.limit;
-          url_ += pagination;
-        }
-        console.log('URL:' + url_);
+        var url_ = '/menu/map_tree';
         v.$api.get(url_, params, function (resp) {
-          v.list = resp.data.items;
-          v.total = resp.data.pagination.total;
-          //父调用子
-          v.$refs.paginator.logs(resp);
+          console.log(resp);
+          v.tree_data = resp.data;
+        })
+      },
+      detail: function (store, data) {
+        console.log(data.id + ':' + data.menu_name + ":" + data.label + ":" + store.label + ":" + store.menu_name);
+      },
+      del: function (store, data) {
+        var v = this;
+        var url_ = '/menu/' + data.id;
+        console.log(data);
+        return;
+        v.$api.delete(url_, {}, function (resp) {
+          v.load_data();
         })
       },
     },
