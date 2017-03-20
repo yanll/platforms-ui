d80687fcbc8ba9a9cc9ed839e7c1fc43291da13d
yanll <m3GAGGM7>
2017-03-20 18:56:21 +0800

    M

--- auth-ui/src/pages/home.vue
+++ auth-ui/src/pages/home.vue
@@ -3,5 +3,20 @@
 </template>
 
 <script>
-  export default {};
+  import VueWebsocket from "vue-websocket";
+  Vue.use(VueWebsocket, "http://localhost:8080", {
+    reconnection: false
+  });
+
+  export default {
+    methods: {},
+
+    socket: {
+      events: {
+        changed(msg) {
+          console.log("Something changed: " + msg);
+        }
+      }
+    }
+  };
 </script>
